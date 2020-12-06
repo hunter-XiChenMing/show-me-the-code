22214b467bac9a80d30414b7d5d5692459b1efb9
hunter-XiChenMing <66580347+hunter-XiChenMing@users.noreply.github.com>
2020-12-06 16:20:32 +0800

    Delete ResourceCommonConst.java

--- util/src/main/java/com/starlib/util/ResourceCommonConst.java
+++
@@ -1,7 +0,0 @@
-package com.starlib.util;
-
-public class ResourceCommonConst {
-
-    public final static MethodReflect<RestResponse> restResponse = new MethodReflect<RestResponse>(RestResponse.Class);
-
-}
